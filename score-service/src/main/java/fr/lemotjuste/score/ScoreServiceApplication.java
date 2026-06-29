6bb005cca73d7c8c1eb68a53b709897584b4b013
Tsouli <93455423+Tsouli-Abde@users.noreply.github.com>
2026-06-29 17:50:57 +0200

    Ajoute le squelette Maven de score-service (pom, Dockerfile, config)

---
+++ score-service/src/main/java/fr/lemotjuste/score/ScoreServiceApplication.java
@@ -0,0 +1,12 @@
+package fr.lemotjuste.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreServiceApplication.class, args);
+    }
+}
